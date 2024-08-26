d07f813fd8b032b65e557efc870a6d7a84ff4849
Andrew Farrell <andrewfarrell@Andrews-MacBook-Pro.local>
2024-08-26 10:56:44 -0400

    Case Expression

---
+++ SQL_Practice_Problems/CaseExpressions.sql
@@ -0,0 +1,15 @@
+-- CASE EXPRESSIONS
+
+SELECT
+    COUNT(job_id) AS job_postings,
+    CASE
+        WHEN job_location = 'Anywhere' THEN 'Remote'
+        WHEN job_location = 'New York, NY' THEN 'Local'
+        ELSE 'Onsite'
+    END AS location_category
+FROM
+    job_postings_fact
+WHERE
+    job_title_short = 'Data Analyst'
+GROUP BY
+    location_category;
